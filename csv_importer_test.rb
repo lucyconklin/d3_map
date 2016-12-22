3fdd414ac53fcc5bbcf96661d35bd239652d02c0
Lucy Conklin <lucy.conklin@gmail.com>
2016-12-22 11:49:13 -0700

    Initial commit

---
+++ csv_importer_test.rb
@@ -0,0 +1,22 @@
+require './importer'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class ImporterTest < Minitest::Test
+  def test_it_will_initialize
+    filename = 'csv/nps_parks.csv'
+    location = 'csv/parks_by_state.csv'
+    importer = NationalParkImporter.new(filename, location)
+
+    assert_equal NationalParkImporter, importer.class
+  end
+
+  def test_it_can_import
+    filename = 'csv/nps_parks.csv'
+    location = 'csv/parks_by_state.csv'
+    importer = NationalParkImporter.new(filename, location)
+    result = importer.import
+
+    assert_equal result, "Hello World"
+  end
+end
---
+++ importer.rb
@@ -0,0 +1,30 @@
+require 'csv'
+require 'pry'
+
+class NationalParkImporter
+  attr_reader :filename, :number_of_lines
+
+  def initialize(filename, location)
+    @filename = filename
+    @number_of_lines = `wc -l #{filename}`.to_i
+    @state_parks = Hash.new(0)
+  end
+
+  def import
+    contents = CSV.open @filename, headers:true, header_converters: :symbol
+    contents.each do |row|
+      row[:state].split(",").each do |state|
+        @state_parks[state] += 1
+      end
+    end
+    make_json(@state_parks)
+  end
+
+  def make_json(state_parks)
+    json = ""
+    state_parks.each do |key, value|
+      json << "#{key},#{value}"+"\n"
+    end
+    json
+  end
+end
